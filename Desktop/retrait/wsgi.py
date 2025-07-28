21448541cfcb99c43a7c4c39093c7387a4ee411b
Gounou Zime <gounouzime50@gmail.com>
2025-07-28 17:16:34 +0200

    Ajout de la création automatique des tables dans wsgi.py

--- Desktop/retrait/wsgi.py
+++ Desktop/retrait/wsgi.py
@@ -1,6 +1,9 @@
 # wsgi.py
 
-from app import create_app
+from app import create_app, db
 
-# Crée l'application Flask
 app = create_app()
+
+# Création automatique des tables à chaque démarrage si elles n'existent pas
+with app.app_context():
+    db.create_all()
